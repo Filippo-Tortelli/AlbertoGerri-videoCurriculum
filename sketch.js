e2707db7cee9fefb6602eb5943a954371fa0f6fa
Filippo-Tortelli <filippo.tortelli@gmail.com>
2020-12-12 22:02:26 +0100

    start

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function preload(){
+  // put preload code here
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+}
